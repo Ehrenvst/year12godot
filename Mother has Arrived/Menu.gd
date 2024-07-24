48b4cf5a0f16a38e9144a2dc37ee5de599ba3e1d
Ehrenvst <ehrvan53@kingshigh.school.nz>
2024-07-24 14:57:16 +1200

    omg is that a title screen

---
+++ Mother has Arrived/Menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+func _ready():
+	$"VBoxContainer/Level 1".grab_focus()
+
+
+func _on_level_1_pressed():
+	get_tree().change_scene_to_file("res://Testing Levels.tscn")
+
+func _on_quit_pressed():
+	get_tree().quit()
